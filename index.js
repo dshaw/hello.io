a4b21963fe662e60c4c5831b375803171856f26e
Daniel Shaw <github@dshaw.com>
2011-08-04 11:58:44 -0700

    A little bit more interaction with for working with Redis instances.

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var http = require('http'), 
-    io = require('socket.io'),
-    port = process.argv[2] || 9000,
+var http = require('http')
+  , io = require('socket.io')
+  , RedisStore = io.RedisStore
+  , port = process.argv[2] || 9000
 
-    server = http.createServer(function(req, res){
+  , server = http.createServer(function(req, res){
       console.log('http request');
       res.writeHead(200, {'Content-Type': 'text/html'});
-      var body = ["<script src='/socket.io/socket.io.js'></script>\n"
+      var body = ["<!DOCTYPE html>\n"
+        , "<script src='/socket.io/socket.io.js'></script>\n"
         , "<script>\n"
-        , "var socket = io.connect();\n"
-        , "socket.on('connect', function(){ console.log('connect') });\n"
-        , "socket.on('message', function(){ console.log('message') });\n"
-        , "socket.on('disconnect', function(){ console.log('disconnect') });\n"
+        , "window.onload = function () {\n"
+        , "  var socket = io.connect(),\n"
+        , "    hello = document.getElementById('hello'),\n"
+        , "    content = function (msg) { document.getElementById('hello').innerHTML += msg; return msg; };\n"
+        , "  hello.onkeyup = function (e) { socket.send(String.fromCharCode(e.keyCode)[(e.shiftKey) ? 'toLocaleUpperCase' : 'toLocaleLowerCase']()); };\n"  
+        , "  socket.on('message', function () { console.log(content(' message ')); });\n"
+        , "  ['connecting','connect','disconnect'].forEach(function(event) {\n"
+        , "    socket.on(event, function () { console.log(content(' '+event+' ')); });\n"
+        , "  });\n"
+        , "};\n"
         , "</script>\n"
-        , "Open your console...\n\n"].join('');
+        , "<body id=hello contentEditable>Open your console...</body>\n\n"].join('');
 
       res.end(body);
    });
@@ -22,6 +30,10 @@ server.listen(port);
 server.on('listening', function () { console.log('Hello.io listening on :%d', port) });
 
 var socket = io.listen(server);
+
+socket.configure(function () {
+  socket.set('store', new RedisStore);
+});
 
 socket.on('connection', function (client) {
   console.log('connected');
